1930b65b9ffea904e241a631c3ae4fe073376617
Lehi Corona <coronalehi@gmail.com>
2025-09-09 20:41:10 -0600

    hero & cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,7 @@
-module.exports = {content: ['./index.html']};
+    // ./tailwindcss -i input.css -o styles.css --watch
+let x = new Date();
+console.log(x);
+let y = x.getFullYear();
+console.log (y);
+document.querySelector('#theYear').textContent = y;
+ Module.exports = {content:'./index.html'}
